b3a2a69f5ab828de8b8d26a07bca0c087feaedd1
Yannick Yanga <cleps2012@gmail.com>
2023-12-20 15:46:25 +0100

    add HashRouter

--- front-project/src/App.tsx
+++ front-project/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/navbar'
 import Login from './Pages/login';
 import Signup from './Pages/signup';
